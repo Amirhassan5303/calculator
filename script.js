7c10ba55d696cc562c11043c86b21f5f0f2b61f6
Amirhassan5303 <amirhassan5303@gmail.com>
2026-05-10 14:34:55 +0200

    Adding appendNumber(), updateDisplay(), getCurrentInput() to script.js

--- script.js
+++ script.js
@@ -1,17 +1,46 @@
 const display = document.getElementById("display");
 const buttons = document.querySelector(".buttons");
 
+let firstNumber = "";
+let secondNumber = "";
+let currentInput;
+let operator = null;
+
+const updateDisplay = function (value) {
+  display.textContent = value;
+};
+
+const getCurrentInput = function () {
+  return display.textContent;
+};
+
+const appendNumber = function (value) {
+  currentInput = getCurrentInput();
+
+  if (value === "." && currentInput.includes(".")) {
+    return;
+  }
+  if (currentInput === "0" && value !== ".") {
+    currentInput = value;
+  } else {
+    currentInput += value;
+  }
+
+  updateDisplay(currentInput);
+};
+
 buttons.addEventListener("click", (event) => {
   const target = event.target;
 
   let value = target.dataset.value;
-  let operation = target.dataset.operation;
+  let operator = target.dataset.operator;
   let action = target.dataset.action;
 
   if (value) {
+    appendNumber(value);
   }
 
-  if (operation) {
+  if (operator) {
   }
 
   if (action) {
