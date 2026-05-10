233dc7c36b0cf255330d903fdf50c34cb7174f6a
Amirhassan5303 <amirhassan5303@gmail.com>
2026-05-10 12:06:26 +0200

    Adding addEventListener to script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 const display = document.getElementById("display");
+const buttons = document.querySelector(".buttons");
+
+buttons.addEventListener("click", (event) => {
+  const target = event.target;
+
+  let value = target.dataset.value;
+  let operation = target.dataset.operation;
+  let action = target.dataset.action;
+
+  if (value) {
+  }
+
+  if (operation) {
+  }
+
+  if (action) {
+  }
+});
